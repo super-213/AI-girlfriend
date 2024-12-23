368c5884fde92b93dccdddf53d3584cb6af2d7ea
super-213 <zhihaojiangzhj@gmail.com>
2024-12-23 20:05:56 +0800

    Initial Commit

---
+++ 看板娘/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  看板娘
+//
+//  Created by 姜智浩 on 2024/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
