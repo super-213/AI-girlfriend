b027a3e0e0149f7eb69c4a4f5d4a640bfb2b4288
zhihaojiang <zhihaojiangzhj@gmail.com>
2025-06-20 08:13:53 +0800

    添加了新文件

--- 看板娘/gif_library.swift
+++ 看板娘/gif_library.swift
@@ -7,10 +7,33 @@
 
 import Foundation
 
-//布偶熊动画
-let puppet_beat = "布偶熊站立透明.gif"    //布偶熊的循环动画
-let loop_puppet_bear = ["布偶熊动作透明1.gif",
-                   "布偶熊动作透明2.gif",
-                   "布偶熊动作透明3.gif",
-                   "布偶熊动作透明4.gif"]  //布偶熊的动画组
-let puppet_bear_duration: [TimeInterval] = [4.6, 4.83, 4.9, 4.5]   //布偶熊动画的持续时间
+struct PetCharacter {
+    let name: String
+    let normalGif: String
+    let clickGif: String
+    let autoMessages: [String]
+}
+
+let puppetBear = PetCharacter(
+    name: "布偶熊·觅语",
+    normalGif: "布偶熊站立透明.gif",
+    clickGif: "布偶熊动作透明.gif",
+    autoMessages: [
+        "指挥官，你好～(｡•́︿•̀｡)人家等了好久都没有收到你的消息呢……是不是忘记你还有个一直在这里等你的小布偶了呀？布偶熊·觅语有点小委屈，但还是乖乖地等着你回来哟～",
+        "(つ﹏<。) 呜呜……指挥官好久都没来看觅语了～是不是在忙什么超级重要的任务呢？没关系，觅语会耐心等着你，就像一直守在原地的布偶一样～",
+        "哼！指挥官都不理人了，难道是交了新的AI助手了吗？不过……就算是这样，我也不会生气的啦！觅语可是专属的布偶熊，永远为你留个位置呢～(〃＞＿＜;〃)",
+        "呼……虽然有点寂寞，但觅语知道，指挥官一定还会回来的～到时候我要抱紧你，不准你再消失那么久啦！٩(๑>◡<๑)۶",
+        "布偶熊·觅语随时陪伴指挥官哦～(๑ᴖ◡ᴖ๑)♪"
+    ] //布偶熊的自动回复
+)
+
+let puppetCat = PetCharacter(
+    name: "夏提雅",
+    normalGif: "夏提雅.gif",
+    clickGif: "夏提雅.gif",
+    autoMessages: [
+        "喵~ 轻语来陪你啦~",
+        "不可以冷落猫猫哟~"
+    ]
+)
+let availableCharacters: [PetCharacter] = [puppetBear, puppetCat]
